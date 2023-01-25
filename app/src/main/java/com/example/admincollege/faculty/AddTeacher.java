43d66e9b5c7b1087a16d71d24b1dde5e1eca1177
Rabbi <Rabbyislam7777@gmail.com>
2023-01-25 16:13:02 +0600

    update faculty design

--- app/src/main/java/com/example/admincollege/faculty/AddTeacher.java
+++ app/src/main/java/com/example/admincollege/faculty/AddTeacher.java
@@ -1,16 +1,86 @@
 package com.example.admincollege.faculty;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.admincollege.R;
 
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class AddTeacher extends AppCompatActivity {
+
+    private CircleImageView addTeacherImage;
+    private EditText addTeacherName,addTeacherEmail,addTeacherPost;
+    private Spinner addTeacherCategory;
+    private Button addTeacherButton;
+    private final int REQ = 1;
+    private Bitmap bitmap;
+    private String category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_teacher);
+
+        addTeacherImage = findViewById(R.id.addTeacherImage);
+        addTeacherName = findViewById(R.id.addTeacherName);
+        addTeacherEmail = findViewById(R.id.addTeacherEmail);
+        addTeacherPost = findViewById(R.id.addTeacherPost);
+        addTeacherCategory = findViewById(R.id.addTeacherCategory);
+        addTeacherButton = findViewById(R.id.addTeacherButton);
+
+        addTeacherImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+        String [] items = new String[]{"Select Category","CSE","SWE","EEE"};
+        addTeacherCategory.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,items));
+
+        addTeacherCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                category = addTeacherCategory.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+    private void openGallery() {
+        Intent pickImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickImage, REQ);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            addTeacherImage.setImageBitmap(bitmap);
+        }
     }
 }
