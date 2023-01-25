34eeb8de6c96e56afc72b4958709754e97506f82
Rabbi <Rabbyislam7777@gmail.com>
2023-01-25 18:20:12 +0600

    upload all teacher data to firebase (complete)

--- app/src/main/java/com/example/admincollege/faculty/AddTeacher.java
+++ app/src/main/java/com/example/admincollege/faculty/AddTeacher.java
@@ -1,8 +1,10 @@
 package com.example.admincollege.faculty;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -14,10 +16,25 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.admincollege.NoticeData;
 import com.example.admincollege.R;
+import com.example.admincollege.UploadNoticeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,13 +45,21 @@ public class AddTeacher extends AppCompatActivity {
     private Spinner addTeacherCategory;
     private Button addTeacherButton;
     private final int REQ = 1;
-    private Bitmap bitmap;
+    private Bitmap bitmap=null;
     private String category;
+    private String name,email,post,downloadUrl="";
+    private ProgressDialog pd;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_teacher);
+
+        pd = new ProgressDialog(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("teacher");
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         addTeacherImage = findViewById(R.id.addTeacherImage);
         addTeacherName = findViewById(R.id.addTeacherName);
@@ -47,6 +72,13 @@ public class AddTeacher extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openGallery();
+            }
+        });
+
+        addTeacherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkvalidation();
             }
         });
 
@@ -65,6 +97,87 @@ public class AddTeacher extends AppCompatActivity {
             }
         });
     }
+
+    private void checkvalidation() {
+        name = addTeacherName.getText().toString();
+        email = addTeacherEmail.getText().toString();
+        post = addTeacherPost.getText().toString();
+
+        if (name.isEmpty()){
+            addTeacherName.setError("Empty!");
+            addTeacherName.requestFocus();
+        } else if (email.isEmpty()){
+            addTeacherEmail.setError("Empty!");
+            addTeacherEmail.requestFocus();
+        }else if (post.isEmpty()){
+            addTeacherPost.setError("Empty!");
+            addTeacherPost.requestFocus();
+        }else if (category.equals("Select Category")){
+            Toast.makeText(this, "Please select a category!", Toast.LENGTH_SHORT).show();
+        }else if (bitmap==null){
+            pd.setMessage("Uploading..");
+            pd.show();
+            insertData();
+        }else{
+            pd.setMessage("Uploading..");
+            pd.show();
+            uploadImage();
+        }
+    }
+    private void insertData() {
+//        databaseReference = databaseReference.child("Notice");
+        final String uniqueKey = databaseReference.push().getKey();
+
+
+        TeacherData teacherData = new TeacherData(name,email,post, downloadUrl,uniqueKey);
+        databaseReference.child(category).child(uniqueKey).setValue(teacherData).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                pd.dismiss();
+                Toast.makeText(AddTeacher.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(AddTeacher.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    private void uploadImage() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        byte[] finalImage = baos.toByteArray();
+        final StorageReference filePath;
+        filePath = storageReference.child("Notice").child(finalImage + "jpg");
+        final UploadTask uploadTask = filePath.putBytes(finalImage);
+        uploadTask.addOnCompleteListener(AddTeacher.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    downloadUrl = String.valueOf(uri);
+                                    insertData();
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    pd.dismiss();
+                    Toast.makeText(AddTeacher.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
     private void openGallery() {
         Intent pickImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(pickImage, REQ);
